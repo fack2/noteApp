8e06b205c58c10b512c2102f781bff81d5b1ad4c
DuaaH <dodohalaiqa.1@gmail.com>
2019-11-19 16:13:26 +0200

    remove promise from query

--- server/database/queries/checkUser.js
+++ server/database/queries/checkUser.js
@@ -6,8 +6,7 @@ const checkUser = email => {
       "select password, user_id, name from users_accounts where email = $1",
       [email]
     )
-    .then(res => res.rows[0])
-    .catch(err => console.log(err))
+
 }
 
 module.exports = checkUser

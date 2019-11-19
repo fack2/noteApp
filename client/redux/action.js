6ff4bb930c0a74c4cc6dd0a7eb5bce768105d5cb
DuaaH <dodohalaiqa.1@gmail.com>
2019-11-19 11:11:53 +0200

    create functions using redux

--- client/redux/action.js
+++ client/redux/action.js
@@ -1,1 +1,17 @@
 //actions
+
+import axios from 'axios'
+
+export const checkLogin = (email, password) => async (dispatch) => {
+  try {
+    const data = await axios.post("http://192.168.13.248:4000/api/login", { email, password })
+    const login = data.data.msg
+    dispatch({
+      type: 'LOGIN',
+      login
+    })
+  }
+  catch {
+    console.log("ee", e);
+  }
+}
--- client/redux/reducer.js
+++ client/redux/reducer.js
@@ -1,6 +1,10 @@
 
 const initialState = {
-
+  email: "",
+  password: "",
+  errorMsg: "",
+  login: false,
+  success: false,
 }
 
 // reducer function
@@ -9,6 +13,11 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 
   switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        login: action.login
+      }
     default: return state
   }
 }

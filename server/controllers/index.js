b597016e207861fb4ab962869ef2c92c2af00973
DuaaH <dodohalaiqa.1@gmail.com>
2019-11-19 16:11:45 +0200

    handle data in back end

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,10 +4,12 @@ const { auth } = require('../middlewares/auth')
 const { checkAuth } = require('./checkAuth')
 const router = express.Router()
 const { getAllCategories } = require('./getAllCategories')
+const logout = require('./logout')
 
 
-router.get('/api/dashboard', getAllCategories)
+router.get('/api/dashboard', auth, getAllCategories)
 router.post('/api/login', login.get)
+router.get('/api/logout', logout.get)
 
 module.exports = router
 
--- server/controllers/login.js
+++ server/controllers/login.js
@@ -3,27 +3,29 @@ const bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken')
 
 exports.get = (req, res) => {
+  console.log("rr");
+
   const { email, password } = req.body
+
   checkUser(email)
     .then(data => {
-      bcrypt.compare(password, data.password, (err, result) => {
-        if (err) {
-          return res.json({ err })
-        }
-        if (result) {
-          const { userId, userName } = data
+      const dddd = data.rows[0]
+      bcrypt.compare(password, dddd.password, (err, result) => {
 
+     
+        if (result) {
+
+          const { user_id, name } = data
           const token = jwt.sign(
-            { email, user_id: userId, name: userName },
-            process.env.SECRET
-          )
+            { email, user_id: user_id, name: name },
+            process.env.SECRET)
           return res
             .cookie('token', token, { maxAge: 99999999 })
-            .json({ msg: 'true', token })
+            .json({ msg: true, token })
         } else {
-          return res.json({ msg: 'false' })
+          return res.json({ msg: false })
         }
       })
     })
-    .catch(err => res.json({ err }))
+    .catch(err => res.json({ msg: false }))
 }
